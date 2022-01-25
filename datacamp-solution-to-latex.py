9951863d42e43e898e6485677369add2de1f0b8a
henrydatei <31316025+henrydatei@users.noreply.github.com>
2022-01-25 16:02:50 +0100

    fixed many mistakes

--- datacamp-solution-to-latex.py
+++ datacamp-solution-to-latex.py
@@ -24,17 +24,21 @@ def makeListUnique(list):
             returnList.append(i)
     return returnList
 
-url = "https://campus.datacamp.com/courses/data-types-for-data-science-in-python/fundamental-data-types?ex=2"
+url = "https://campus.datacamp.com/courses/ai-fundamentals/introduction-to-ai?ex=4"
 req = requests.get(url)
-result = html.unescape(re.search("window.PRELOADED_STATE = (.*)</script>", req.text).group(1)[:-1])
+result = html.unescape(re.search("window.PRELOADED_STATE = (.*)</script>", req.text).group(1)[:-1]).replace('\\"', "'").replace("\\'", "'").replace('\\\\t', '  ')
 
 previousChapter = ""
 latexString = ""
+title = ""
 
 urls = [url.split("]")[0] for url in findString(result, "url")]
+
+#for line in findString(result, "solution")[2].split('\\\\n'):
+#    print(line)
 for url in makeListUnique(urls):
     req = requests.get(url)
-    result = html.unescape(re.search("window.PRELOADED_STATE = (.*)</script>", req.text).group(1)[:-1])
+    result = html.unescape(re.search("window.PRELOADED_STATE = (.*)</script>", req.text).group(1)[:-1]).replace('\\"', "'").replace("\\'", "'").replace('\\\\t', '  ')
 
     # process information from URL
     titleraw = url.split("/")[4].replace("-", " ")
@@ -43,13 +47,28 @@ for url in makeListUnique(urls):
     exercise = int(parse_qs(parsed_url.query)['ex'][0])
 
     # get solution
-    solLines = findString(result, "solution")[exercise - 1].split('\\\\n')
+    solLines = [line for line in findString(result, "solution")[exercise - 1].split('\\\\n')]
 
     # find exact titles for exercise
-    titles = findString(result, "title")
-    title = difflib.get_close_matches(titleraw, titles)[0]
-    chapter = difflib.get_close_matches(chapterraw, titles)[0]
-    exerciseTitle = titles[titles.index(chapter) + exercise]
+    titles = list(filter(lambda a: a != 'null', findString(result, "title")))
+    try:
+        title = difflib.get_close_matches(titleraw, titles)[0]
+    except:
+        if title == "":
+            title = input("Title: ")
+    try:
+        chapter = difflib.get_close_matches(chapterraw, titles)[0]
+    except:
+        chapter = "unknown"
+    try:
+        exerciseTitle = titles[titles.index(chapter) + exercise]
+    except:
+        exerciseTitle = "unknown"
+
+    #print(titles)
+    print(title + " - " + chapter + " - " + exerciseTitle + " (" + str(exercise) + ")" + " - " + url)
+    for line in solLines:
+        print(line)
 
     # generate Latex
     if solLines[0] != '':
@@ -68,4 +87,4 @@ with open("template.tex", "rt") as fin:
         with open(title + ".tex", "wt") as fout:
             for line in fin:
                 fout.write(line.replace('{title}', str(title))
-                .replace('{solutions}', str(latexString)))
+                .replace('{solutions}', str(latexString.replace("&", "\&"))))
